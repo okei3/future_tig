4c30b3053a77a40f85db6c0204d455d1bd580e1e
Koichi Osanai <osanai3@gmail.com>
2013-06-23 01:08:06 +0900

    add login form

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var http_auth = auth({
     authFile : 'conf/developers'
 });
 
+app.use(express.cookieParser());
+app.use(express.session({secret:'6CQAorGwCNt4XfjhBsQF0g3whwl9gWYw'}));
+app.use(express.bodyParser());
+
 app.configure('development', function () {
     app.all('*', function (req, res, next) {
         http_auth.apply(req, res, function (username) {
@@ -15,11 +19,31 @@ app.configure('development', function () {
     });
 });
 
-app.get('/', function(req,  res) {
+app.get('/', function (req, res) {
     res.sendfile('index.html');
+});
+
+app.post('/login', function (req, res) {
+    var id = req.body.id;
+    var password = req.body.password;
+    if (id === undefined || password === undefined) {
+        res.redirect('/');
+    } else {
+        var user_id = null;//tmp
+        if (user_id === null) {
+            res.redirect('/');
+        } else {
+            req.session.user_id = user_id;
+            req.session.regenerate(function (err) {
+                if (err) {
+                    res.redirect('/');
+                }
+                res.redirect('/index?user_id=' + user_id);
+            });
+        }
+    }
 });
 
 app.configure('development', function () {
     app.listen(8080);
-    console.log('listening');
 });
