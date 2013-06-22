27f2b285953b939a1fafae0fdcc7823e5218752a
Koichi Osanai <osanai3@gmail.com>
2013-06-22 22:12:52 +0900

    add digest auth

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
+var auth = require('http-auth');
+var http_auth = auth({
+    authRealm : 'developer',
+    authType : 'digest',
+    authFile : 'conf/developers'
+});
+
+app.configure('development', function () {
+    app.all('*', function (req, res, next) {
+        http_auth.apply(req, res, function (username) {
+            next();
+        });
+    });
+});
 
 app.get('/', function(req,  res) {
     res.sendfile('index.html');
 });
 
-app.listen(8080);
+app.configure('development', function () {
+    app.listen(8080);
+    console.log('listening');
+});
