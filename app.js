2e5782f647515b6f4074b751eccfc8ecc9ef4c30
Koichi Osanai <osanai3@gmail.com>
2013-06-24 01:09:54 +0900

    add socket.io

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var http_auth = auth({
 app.use(express.cookieParser());
 app.use(express.session({secret:'6CQAorGwCNt4XfjhBsQF0g3whwl9gWYw'}));
 app.use(express.bodyParser());
+app.use(express.static(__dirname + '/public'));
 
 app.configure('development', function () {
     app.all('*', function (req, res, next) {
@@ -20,6 +21,13 @@ app.configure('development', function () {
 });
 
 require('./routes')(app, require('./module'));
+
+app.http().io();
+app.io.route('ready', function (req) {
+    req.io.emit('identify', {
+        access_user_id : req.session.user_id
+    });
+});
 
 app.configure('development', function () {
     app.listen(8080);
