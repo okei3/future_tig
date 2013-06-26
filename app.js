cd383c711eebe3441172951a777fa743fa7bc618
Koichi Osanai <osanai3@gmail.com>
2013-06-26 22:32:23 +0900

    apply SOP

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 var express = require('express.io');
 var app = express();
-var auth = require('http-auth');
-var http_auth = auth({
-    authRealm : 'developer',
-    authType : 'digest',
-    authFile : 'conf/developers'
+
+var config;
+app.configure('development', function () {
+    config = require('./conf/development');
 });
 
 app.use(express.cookieParser());
-app.use(express.session({secret:'6CQAorGwCNt4XfjhBsQF0g3whwl9gWYw'}));
+app.use(express.session({secret : config.sessionSecret}));
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 
 app.configure('development', function () {
+    var auth = require('http-auth');
+    var http_auth = auth({
+        authRealm : 'developer',
+        authType : 'digest',
+        authFile : 'conf/developers'
+    });
     app.all('*', function (req, res, next) {
         http_auth.apply(req, res, function (username) {
             next();
@@ -22,9 +27,10 @@ app.configure('development', function () {
 
 var modules = require('./module');
 app.http().io();
+app.io.set('origins', config.host + ':' + config.port);
 require('./routes')(app, modules);
 require('./io_routes')(app.io, modules);
 
 app.configure('development', function () {
-    app.listen(8080);
+    app.listen(config.port);
 });
