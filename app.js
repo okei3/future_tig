eeca2f48131e4faa8a0a6771b144040473eec844
Koichi Osanai <osanai3@gmail.com>
2013-06-28 02:56:17 +0900

    extract app setting

--- app.js
+++ app.js
@@ -1,38 +1,12 @@
 var config = require('./conf/' + process.env.NODE_ENV);
 var dsn = require('./dsn')(config.dsn);
+var modules = require('./module');
 dsn.redis('main', function (redis) {
-    var express = require('express.io');
-    var app = express();
-    app.http().io();
-    app.use(express.cookieParser());
-    app.use(express.session({
-        store : new (require('connect-redis')(express))({
-            client: redis
-        }),
-        secret : config.sessionSecret
-    }));
-    app.use(express.bodyParser());
-    app.use(express.static(__dirname + '/public'));
-
-    app.configure('development', function () {
-        var auth = require('http-auth');
-        var http_auth = auth({
-            authRealm : 'developer',
-            authType : 'digest',
-            authFile : 'conf/developers'
-        });
-        app.all('*', function (req, res, next) {
-            http_auth.apply(req, res, function (username) {
-                next();
-            });
-        });
+    var app = require('./init')({
+        config : config,
+        sessionRedis : redis
     });
-
-    var modules = require('./module');
-
-    app.io.set('origins', config.domain + ':' + config.port);
     require('./routes')(app, modules);
     require('./io_routes')(app.io, modules);
-
     app.listen(config.port);
 });
---
+++ init/index.js
@@ -0,0 +1,33 @@
+module.exports = function (option) {
+    var express = require('express.io');
+    var app = express();
+    app.http().io();
+    app.use(express.cookieParser());
+    app.use(express.session({
+        store : new (require('connect-redis')(express))({
+            client: option.sessionRedis
+        }),
+        secret : option.config.sessionSecret
+    }));
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/../public'));
+
+    app.configure('development', function () {
+        var auth = require('http-auth');
+        var http_auth = auth({
+            authRealm : 'developer',
+            authType : 'digest',
+            authFile : 'conf/developers'
+        });
+        app.all('*', function (req, res, next) {
+            http_auth.apply(req, res, function (username) {
+                next();
+            });
+        });
+    });
+    app.io.set(
+        'origins',
+        option.config.domain + ':' + option.config.port
+    );
+    return app;
+};
