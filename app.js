aaa1839c6e6a5b6e674bc5c279652e77dd3d38ee
Koichi Osanai <osanai3@gmail.com>
2013-06-28 02:29:38 +0900

    move code into callback

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express.io');
 var app = express();
-
+app.http().io();
 var config;
 app.configure('development', function () {
     config = require('./conf/development');
@@ -15,30 +15,30 @@ dsn.redis('main', function (redis) {
         store : new RedisStore({client: redis}),
         secret : config.sessionSecret
     }));
-});
-app.use(express.bodyParser());
-app.use(express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/public'));
 
-app.configure('development', function () {
-    var auth = require('http-auth');
-    var http_auth = auth({
-        authRealm : 'developer',
-        authType : 'digest',
-        authFile : 'conf/developers'
-    });
-    app.all('*', function (req, res, next) {
-        http_auth.apply(req, res, function (username) {
-            next();
+    app.configure('development', function () {
+        var auth = require('http-auth');
+        var http_auth = auth({
+            authRealm : 'developer',
+            authType : 'digest',
+            authFile : 'conf/developers'
+        });
+        app.all('*', function (req, res, next) {
+            http_auth.apply(req, res, function (username) {
+                next();
+            });
         });
     });
-});
 
-var modules = require('./module');
-app.http().io();
-app.io.set('origins', config.host + ':' + config.port);
-require('./routes')(app, modules);
-require('./io_routes')(app.io, modules);
+    var modules = require('./module');
 
-app.configure('development', function () {
-    app.listen(config.port);
+    app.io.set('origins', config.host + ':' + config.port);
+    require('./routes')(app, modules);
+    require('./io_routes')(app.io, modules);
+
+    app.configure('development', function () {
+        app.listen(config.port);
+    });
 });
