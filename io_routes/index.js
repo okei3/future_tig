fc4ae9999e5ff23829cf9d7386d3e937cb73d30f
Takeshi Kunimi <takc923@gmail.com>
2013-07-02 01:54:34 +0900

    chat とりあえず動いた

--- io_routes/index.js
+++ io_routes/index.js
@@ -1,3 +1,4 @@
 module.exports = function (io, modules) {
     require('./ready')(io, modules);
+    require('./chat')(io, modules);
 };
---
+++ public/javascripts/chat.js
@@ -0,0 +1,22 @@
+var chatId = location.pathname.match(/([0-9]+)\/?$/)[1];
+window.onload = function(){
+    var socket = io.connect(location.origin);
+    socket.json.emit("init", {chatId: chatId});
+    socket.on('chat-stream', function (data) {
+        console.log(data);
+        var messageElement = document.createElement("div");
+        messageElement.innerHTML = data.user + ": " + data.message;
+        document.getElementById("message-container").appendChild(messageElement);
+    });
+
+    var chatForm = document.getElementById("chat-form");
+    chatForm.onkeypress = function(evt) {
+        if(evt.keyCode == 13){
+            socket.emit(
+                'chat-send',
+                { message: chatForm.value, chatId: chatId}
+            );
+            chatForm.value = "";
+        }
+    };
+};
