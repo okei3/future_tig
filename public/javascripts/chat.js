76cadc937eb9def44dd23f205f1f524ea00ac107
Takeshi Kunimi <takc923@gmail.com>
2013-07-05 03:24:48 +0900

    modify io_route endpoint

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,8 +1,8 @@
 var chatId = location.pathname.match(/([0-9]+)\/?$/)[1];
 window.onload = function(){
     var socket = io.connect(location.origin);
-    socket.json.emit("init", {chatId: chatId});
-    socket.on('chat-stream', function (data) {
+    socket.json.emit("chat/init", {chatId: chatId});
+    socket.on('chat/stream', function (data) {
         console.log(data);
         var messageElement = document.createElement("div");
         messageElement.innerHTML = data.user + ": " + data.message;
@@ -13,7 +13,7 @@ window.onload = function(){
     chatForm.onkeypress = function(evt) {
         if(evt.keyCode == 13){
             socket.emit(
-                'chat-send',
+                'chat/send',
                 { message: chatForm.value, chatId: chatId}
             );
             chatForm.value = "";
